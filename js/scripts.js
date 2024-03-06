33da06c5f497a1052a5d7425d86d606cfc3b9f95
sdncoss <157067345+sdncoss@users.noreply.github.com>
2024-03-05 16:07:42 -0800

    cleared error to call correct id

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,40 @@
 function newItem() {
-
-    //jQuery
-    //Adding a new item to the list
-    let li = $('<li></li>');
-    let inputValue = $('#input').val();
-    li.append(inputValue);
-
-    if (inputValue === '') {
-        alert("You must write something!");
-    } else {
-        $('list').append(li);
-    }
-
-    //Crossing out item
-    function crossOut() {
-        li.toggleClass("strike");
-    }
-
-    li.on("dblclick", function crossOut() {
-        li.toggleClass("strike");
-    });
-
-    // Adding delete button "X"
-    let deleteButton = $('<deleteButton></deleteButton>');
-    deleteButton.append(document.createTextNode('X'));
-    li.append(deleteButton);
-
-    deleteButton.on("click", deleteListItem);
-
-    //Adding Class Delete to display: none
-    function deleteListItem() {
-        li.addClass("delete")
-    }
-
-
-    // Reorder Items
-    $('#list').sortable();
+    
+        //jQuery
+        //Adding a new item to the list
+        let li = $('<li></li>');
+        let inputValue = $('#input').val();
+        li.append(inputValue);
+    
+        if (inputValue === '') {
+            alert("You must write something!");
+        } else {
+            $('#list').append(li);
+        }
+    
+        //Crossing out item
+        function crossOut() {
+            li.toggleClass("strike");
+        }
+    
+        li.on("dblclick", function crossOut() {
+            li.toggleClass("strike");
+        });
+    
+        // Adding delete button "X"
+        let deleteButton = $('<deleteButton></deleteButton>');
+        deleteButton.append(document.createTextNode('X'));
+        li.append(deleteButton);
+    
+        deleteButton.on("click", deleteListItem);
+    
+        //Adding Class Delete to display: none
+        function deleteListItem() {
+            li.addClass("delete")
+        }
+    
+    
+        // Reorder Items
+        $('#list').sortable();
+        
 }
-
